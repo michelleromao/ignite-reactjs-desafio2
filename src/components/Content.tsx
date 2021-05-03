6e50edcfb89da30fb198b86e2adcdfd207628eba
Michelle Romão <michelle.nunes10@gmail.com>
2021-05-02 22:59:05 -0300

    Desafio 2 Ignite ReactJS

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+interface Ratings{
+  Source: string;
+  Value: string;
+}
+interface Movie{
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Runtime: string;
+  Ratings: Ratings[];
+}
+interface ContentProps{
+  selectedGenre:{
+    title: string;
+  },
+  movies: Movie[]
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {props.selectedGenre.title} </span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {props.movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
